0f794d20b23fcb142e39ba55a31c422ffb7bec61
Virginie17 <niniezouk@hotmail.fr>
2024-07-27 16:11:14 +0200

    suppression console.log

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -11,25 +11,24 @@ const PER_PAGE = 9;
 
 const EventList = () => {
   const { data, error } = useData();
-  const [type, setType] = useState();
+  const [type, setType] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
-  // filtre sur le type d 'evenements
-  const filteredEvents = ( data?.events || [] ).filter((event) => !type || event.type === type);
-// console.log("evenements", filteredEvents);
-// console.log("type initial", type);
+  // filtre sur le type d'evenement
+  const filteredEvents = (data?.events || []).filter(
+    (event) => !type || event.type === type
+  );
   // Mise en place de la pagination
-  const page = filteredEvents.slice(
+  const paginatedEvents = filteredEvents.slice(
     (currentPage - 1) * PER_PAGE,
     currentPage * PER_PAGE
   );
+
   const changeType = (evtType) => {
-   // console.log("type selectionner", evtType);
     setCurrentPage(1);
     setType(evtType);
   };
   const pageNumber = Math.ceil(filteredEvents.length / PER_PAGE);
   const typeList = new Set(data?.events.map((event) => event.type));
-
   return (
     <>
       {error && <div>An error occured</div>}
@@ -40,16 +39,16 @@ const EventList = () => {
           <h3 className="SelectTitle">Catégories</h3>
           <Select
             selection={Array.from(typeList)}
-            onChange={(value) => (value ? changeType(value) : changeType(null))}
+            onChange={(value) => changeType(value || null)}
           />
           <div id="events" className="ListContainer">
-            {page.map((event) => (
+            {paginatedEvents.map((event) => (
               <Modal key={event.id} Content={<ModalEvent event={event} />}>
                 {({ setIsOpened }) => (
                   <EventCard
                     onClick={() => setIsOpened(true)}
-                    imageSrc={event.cover}
-                    title={event.title}
+                    imageSrc={event?.cover}
+                    title={event?.title}
                     date={new Date(event.date)}
                     label={event.type}
                   />
